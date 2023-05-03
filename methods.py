6877e04e76b265dbc5729cd417cc230b36ce44cd
LUCAS MACHADO DA COSTA PENNONE <21.01086-2@maua.br>
2023-05-03 09:15:12 -0300

    solução ex calculadora'
    Signed-off-by: LUCAS MACHADO DA COSTA PENNONE <21.01086-2@maua.br>

--- methods.py
+++ methods.py
@@ -5,3 +5,27 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def area_triangulo(windth, height):
+    areat = (windth*height)/2
+    return areat
+
+def area_quadrado(windth):
+    area = 2*windth
+    return area
+
+def soma(valor1, valor2):
+    s = valor1 + valor2
+    return soma  
+
+def subtracao(v1, v2):
+    sub = v1 - v2
+    return sub
+
+def multiplicacao(v1, v2):
+    m = v1 * v2
+    return m
+
+def divisao(v1, v2):
+    d = v1 / v2
+    return d              
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,73 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_area_triangulo(windth, height):
+    # given a width of 2 and a height of 5
+    width = 2
+    height = 5
+
+    # when we calculate the perimeter
+    output = methods.area_triangulo(width, height)
+    
+    # then the perimeter should be 10
+    assert output == 10
+
+def test_area_quadrado(windth):
+    # given a width of 2 
+    width = 2
+  
+
+    # when we calculate the perimeter
+    output = methods.area_quadrado(width)
+    
+    # then the perimeter should be 4
+    assert output == 4
+
+def test_soma(valor1, valor2)
+    #given a value1 of 5 and a value2 of 3
+    valor1 = 5
+    valor2 = 3
+
+
+    #when we calculate the sum
+    output = methods.soma(valor1, valor2)
+
+    #then the result should be 8
+    assert output = 8
+
+def test_subtracao(v1, v2)
+    #given a value1 of 5 and a value2 of 3
+    v1 = 5
+    v2 = 3
+
+
+    #when we calculate the subtraction
+    output = methods.subtracao(v1, v2)
+
+    #then the result should be 2
+    assert output = 2
+
+def test_multiplicacao(v1, v2)
+    #given a value1 of 5 and a value2 of 3
+    v1 = 5
+    v2 = 3
+
+
+    #when we calculate the multiplication
+    output = methods.multiplicacao(v1, v2)
+
+    #then the result should be 15
+    assert output = 15
+
+def test_divisao(v1, v2)
+    #given a value1 of 10 and a value2 of 2
+    v1 = 10
+    v2 = 2
+
+
+    #when we calculate the division
+    output = methods.divisao(v1, v2)
+
+    #then the result should be 5
+    assert output = 5  
